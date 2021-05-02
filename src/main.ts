0d5bcd659c0e3d37df7695f269904e7e74224888
Christopher Yu <christophergin88@gmail.com>
2021-05-02 17:28:33 +0800

    implements express-session in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'asd',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
