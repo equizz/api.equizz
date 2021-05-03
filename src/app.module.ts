45d44ac9a625066f3cc7e10feaef4b25218440e7
Christopher Yu <christophergin88@gmail.com>
2021-05-04 02:41:28 +0800

    added new line for easier code read

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,11 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://db_mongo/equizz'), AuthModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://db_mongo/equizz'),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
