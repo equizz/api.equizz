bd6bf9679b2f16643670ad09a3269ae772421deb
Christopher Yu <christophergin88@gmail.com>
2021-05-04 02:40:39 +0800

    added create user authorization using jwt guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,22 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import * as bcrypt from 'bcrypt';
+import { Role } from 'src/enums/user.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto, @Request() req) {
+    const { role } = req.user;
+
+    if (role === Role.Siswa) throw new UnauthorizedException();
+
+    if (role === Role.Guru && createUserDto.role !== Role.Siswa)
+      throw new UnauthorizedException();
+
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
+    console.log(await bcrypt.hash('bodoh', 12));
     return this.usersService.findAll();
   }
 
