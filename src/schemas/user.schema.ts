0362c31b688f12aef7b3352811c9df3635332984
Christopher Yu <christophergin88@gmail.com>
2021-05-04 02:40:20 +0800

    updated user role enum import loc

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { Role } from './enums/user.enum';
+import { Role } from '../enums/user.enum';
 
 export type UserDocument = User & Document;
 
