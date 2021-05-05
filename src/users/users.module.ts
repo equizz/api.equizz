669702071a9a94ecd58b414f185fa3c69f2db101
Christopher Yu <christophergin88@gmail.com>
2021-05-06 01:07:15 +0800

    add mongoose setup to variable (soon will make a different file for this)

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,34 +5,35 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/user.schema';
 import * as bcrypt from 'bcrypt';
 
-@Module({
-  imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: User.name,
-        useFactory: () => {
-          const schema = UserSchema;
+const mongooseSetup = MongooseModule.forFeatureAsync([
+  {
+    name: User.name,
+    useFactory: () => {
+      const schema = UserSchema;
 
-          schema.pre('save', async function (done) {
-            if (this.isModified('password')) {
-              const hashed = await bcrypt.hash(this.get('password'), 12);
-              this.set('password', hashed);
-            }
-            done();
-          });
+      schema.pre('save', async function (done) {
+        if (this.isModified('password')) {
+          const hashed = await bcrypt.hash(this.get('password'), 12);
+          this.set('password', hashed);
+        }
+        done();
+      });
 
-          schema.set('toJSON', {
-            transform: (doc, ret) => {
-              delete ret.password;
-            },
-          });
-
-          return schema;
+      schema.set('toJSON', {
+        transform: (doc, ret) => {
+          delete ret.password;
         },
-      },
-    ]),
-  ],
+      });
+
+      return schema;
+    },
+  },
+]);
+
+@Module({
+  imports: [mongooseSetup],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [mongooseSetup],
 })
 export class UsersModule {}
